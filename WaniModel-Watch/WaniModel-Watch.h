6fe99dff08c53ec88011ff8c6873c877b5890b6f
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-03-23 18:02:56 +0200

    first commit

---
+++ WaniModel-Watch/WaniModel-Watch.h
@@ -0,0 +1,19 @@
+//
+//  WaniModel-Watch.h
+//  WaniModel-Watch
+//
+//  Created by Andriy K. on 3/23/17.
+//  Copyright © 2017 @haawa799. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for WaniModel-Watch.
+FOUNDATION_EXPORT double WaniModel_WatchVersionNumber;
+
+//! Project version string for WaniModel-Watch.
+FOUNDATION_EXPORT const unsigned char WaniModel_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaniModel_Watch/PublicHeader.h>
+
+
---
+++ WaniModel/WaniModel.h
@@ -0,0 +1,19 @@
+//
+//  WaniModel.h
+//  WaniModel
+//
+//  Created by Andriy K. on 3/23/17.
+//  Copyright © 2017 @haawa799. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WaniModel.
+FOUNDATION_EXPORT double WaniModelVersionNumber;
+
+//! Project version string for WaniModel.
+FOUNDATION_EXPORT const unsigned char WaniModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaniModel/PublicHeader.h>
+
+
